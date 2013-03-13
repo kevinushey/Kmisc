696775ad5844e744557392e36ac0ef03a697b35f
Kevin Ushey <kevinushey@gmail.com>
2013-03-13 00:38:00 -0700

    proper handling of integers/logicals in any_na; small tests

---
+++ Kmisc/inst/tests/test-any_na.R
@@ -0,0 +1,13 @@
+library(testthat)
+
+x <- rnorm(10)
+x[1] <- NA
+expect_identical( any( is.na(x) ), any_na(x) )
+x <- as.character(x)
+expect_identical( any( is.na(x) ), any_na(x) )
+x <- as.integer(x)
+expect_identical( any( is.na(x) ), any_na(x) )
+x <- as.factor(x)
+expect_identical( any( is.na(x) ), any_na(x) )
+x <- as.logical(x)
+expect_identical( any( is.na(x) ), any_na(x) )
--- Kmisc/src/any_na.c
+++ Kmisc/src/any_na.c
@@ -2,29 +2,50 @@
 #include <Rinternals.h>
 #include <Rdefines.h>
 
-#define HANDLE_CASE( RTYPE, CTYPE, ACCESSOR ) \
-	case RTYPE: { \
-		CTYPE* ptr = ACCESSOR(x); \
-		for( int i=0; i < len; ++i ) { \
-			if( ISNA( ptr[i] ) || ISNAN( ptr[i] ) ) { \
-				LOGICAL(out)[0] = TRUE; \
-				UNPROTECT(1); \
-				return out; \
-			} \
-		} \
-		LOGICAL(out)[0] = FALSE; \
-		UNPROTECT(1); \
-		return out; \
-	} \
-
 SEXP any_na( SEXP x ) {
 	SEXP out;
 	PROTECT(out = NEW_LOGICAL(1));
 	int len = Rf_length(x);
 	switch( TYPEOF(x) ) {
-	HANDLE_CASE( INTSXP, int, INTEGER )
-	HANDLE_CASE( REALSXP, double, REAL )
-	HANDLE_CASE( LGLSXP, int, LOGICAL )
+	case REALSXP: {
+		double* ptr = REAL(x);
+		for( int i=0; i < len; ++i ) {
+			if( ISNA( ptr[i] ) || ISNAN( ptr[i] ) ) {
+				LOGICAL(out)[0] = TRUE;
+				UNPROTECT(1);
+				return out;
+			}
+		}
+		LOGICAL(out)[0] = FALSE;
+		UNPROTECT(1);
+		return out;
+	}
+	case INTSXP: {
+		int* ptr = INTEGER(x);
+		for( int i=0; i < len; ++i ) {
+			if( ptr[i] == NA_INTEGER ) {
+				LOGICAL(out)[0] = TRUE;
+				UNPROTECT(1);
+				return out;
+			}
+		}
+		LOGICAL(out)[0] = FALSE;
+		UNPROTECT(1);
+		return out;
+	}
+	case LGLSXP: {
+		int* ptr = LOGICAL(x);
+		for( int i=0; i < len; ++i ) {
+			if( ptr[i] == NA_LOGICAL ) {
+				LOGICAL(out)[0] = TRUE;
+				UNPROTECT(1);
+				return out;
+			}
+		}
+		LOGICAL(out)[0] = FALSE;
+		UNPROTECT(1);
+		return out;
+	}
 	case STRSXP: {
 		for( int i=0; i < len; ++i ) {
 			if( STRING_ELT(x, i) == NA_STRING ) {
