8d53e13e7ef6d63809ec2773fffe26ccf187c46d
Kevin Ushey <kevinushey@gmail.com>
2013-09-03 14:13:16 -0700

    wrap error cases in expect_error

--- Kmisc/inst/tests/test-melt_.R
+++ Kmisc/inst/tests/test-melt_.R
@@ -139,8 +139,8 @@ tmp2 <- melt(DF, id=c("x1", "x4"), measure="x2")
 tmp2$variable <- as.character(tmp2$variable)
 expect_identical(tmp1, tmp2)
 
-tmp3 <- melt_(DF, id=c("x1", "x4"), measure=c("x5", "x6"))
-tmp4 <- melt(DF, id=c("x1", "x4"), measure=c("x5", "x6"))
+expect_error(tmp3 <- melt_(DF, id=c("x1", "x4"), measure=c("x5", "x6")))
+expect_error(tmp4 <- melt(DF, id=c("x1", "x4"), measure=c("x5", "x6")))
 
 ## matrix tests
 x <- matrix(1:24, nrow=4)
