ab84959b8c6c936eacffcbb228ec64127fe57966
Kevin Ushey <kevinushey@gmail.com>
2013-09-11 10:29:21 -0700

    str_split gets new name, split2df, to avoid stringr mask

--- R/str_split.R
+++ R/str_split.R
@@ -4,6 +4,12 @@
 ##' structure, and converts that vector into a \code{data.frame}, split
 ##' on that delimiter. A nice wrapper to \code{\link{strsplit}}, essentially 
 ##' - the primary bonus is the automatic coersion to a \code{data.frame}.
+##' 
+##' Note that the preferred method for reading text data with a single, one
+##' character delimiter is through \code{read.table(text=...)} or
+##' \code{data.table::fread}; however, this function is helpful in the case of
+##' non-regular delimiters (that you wish to specify with a regex)
+##' 
 ##' @param x a vector of strings.
 ##' @param sep the delimiter / \code{\link{regex}} you wish to split your strings on.
 ##' @param fixed logical. If \code{TRUE}, we match \code{sep} exactly; 
@@ -25,24 +31,22 @@
 ##' str_split( x, "[_\\.]", names=c("first", "second", "third") )
 str_split <- function(x, sep, fixed=FALSE, perl=TRUE, useBytes=FALSE, names=NULL) {
   
-  if( fixed ) perl <- FALSE
+  if (fixed) perl <- FALSE
   
   x <- as.character(x)
   tmp <- strsplit( x, sep, fixed=fixed, perl=perl, useBytes=useBytes )
-  if( length( unique( sapply( tmp, length ) ) ) > 1 ) {
+  if (length( unique( sapply( tmp, length ) )) > 1 ) {
     stop("non-equal lengths for each entry of x post-splitting")
   }
-#   tmp <- unlist(tmp)
-#   tmp <- as.data.frame( matrix( tmp, ncol=length(tmp)/length(x), byrow=TRUE ),
-#                         stringsAsFactors=FALSE, optional=TRUE
-#   )
   tmp <- .Call( "charlist_transpose_to_df", tmp, PACKAGE="Kmisc" )
   if( !is.null(names) ) {
     names(tmp) <- names
   } else {
     names(tmp) <- paste( "V", 1:ncol(tmp), sep="" )
   }
-  
   return(tmp)
-  
 }
+
+##' @rdname str_split
+##' @export
+split2df <- str_split
