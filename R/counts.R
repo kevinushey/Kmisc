cae3840294b98685eaac1b8705fb40e5307f7b7d
Kevin Ushey <kevinushey@gmail.com>
2013-11-03 19:20:23 -0800

    doc update

--- R/counts.R
+++ R/counts.R
@@ -3,7 +3,9 @@
 ##' This function uses Rcpp sugar to implement a fast \code{table}, for
 ##' unique counts of a single vector.
 ##' 
-##' @param x A numeric, integer, or character vector.
+##' @param x A numeric, integer, character or logical vector, or a (potentially
+##'   nested) list of such vectors. If \code{x} is a list, we recursively apply
+##'   counts throughout elements in the list.
 ##' @export
 counts <- function(x) {
   if( is.list(x) ) {
