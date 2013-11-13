56754f3ae1203e005d5921f9a6333a7953c23e1d
Kevin Ushey <kevinushey@gmail.com>
2013-11-13 10:43:31 -0800

    transpose_list -> S3 generic transpose

---
+++ R/transpose.R
@@ -0,0 +1,31 @@
+##' Tranpose an Object
+##' 
+##' This functions similarily to \R's \code{t}, but we add a new method,
+##' \code{transpose.list}, for transposing lists in a specific way.
+##' 
+##' @param x A matrix, data.frame, or matrix-like list.
+##' @export
+transpose <- function(x) {
+  UseMethod("transpose")
+}
+
+##' @rdname transpose
+##' @S3method transpose list
+##' @method transpose list
+transpose.list <- function(x) {
+  return( .Call("transpose_list", as.list(x), PACKAGE="Kmisc") )
+}
+
+##' @rdname transpose
+##' @S3method transpose data.frame
+##' @method transpose data.frame
+transpose.data.frame <- function(x) {
+  return( t.data.frame(x) )
+}
+
+##' @rdname transpose
+##' @S3method transpose default
+##' @method transpose default
+transpose.default <- function(x) {
+  t.default(x)
+}
