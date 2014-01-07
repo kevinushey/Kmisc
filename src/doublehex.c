3c49ff97e82954bfff40edcfddd278bf51267e0d
Kevin Ushey <kevinushey@gmail.com>
2014-01-07 15:33:20 -0800

    make doublehex more useful

--- src/doublehex.c
+++ src/doublehex.c
@@ -3,7 +3,23 @@
 
 // [[export]]
 SEXP double2hex(SEXP x) {
-  unsigned long *xx = (unsigned long*) REAL(x);
-  Rprintf("%02llX", *xx);
-  return R_NilValue;
+  
+  if (sizeof(unsigned long long) != sizeof(double)) {
+    error("this code assumes sizeof(unsigned long long) and sizeof(double) are identical");
+  }
+  
+  // double is 8 bytes, each byte can be represented by 2 hex chars,
+  // so need a str with 16+1 slots
+  int n = sizeof(unsigned long long) * 2 + 1;
+  
+  unsigned long long *xx = (unsigned long long*) REAL(x);
+  char buf[n];
+  for (int i=0; i < n-1; ++i) {
+    buf[i] = '0';
+  }
+  snprintf(buf, n, "%llX", *xx);
+  SEXP output = PROTECT(allocVector(STRSXP, 1));
+  SET_STRING_ELT(output, 0, mkChar(buf));
+  UNPROTECT(1);
+  return output;
 }
