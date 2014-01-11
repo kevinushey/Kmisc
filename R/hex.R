620c7edd54ff3d03da6888204b52fd67db03a2e9
Kevin Ushey <kevinushey@gmail.com>
2014-01-10 23:46:13 -0800

    extend hex to int, logical

--- R/hex.R
+++ R/hex.R
@@ -1,5 +1,7 @@
 ##' Get the hex representation of a value
 ##' 
+##' Prints the hex (byte) representation of a value.
+##' 
 ##' @param x A value.
 ##' @export
 hex <- function(x) {
@@ -8,5 +10,15 @@ hex <- function(x) {
 
 ##' @export
 hex.double <- function(x) {
-  .Call(Cdouble2hex, x)
+  vapply(x, FUN.VALUE=character(1), function(xx) .Call(Cdouble2hex, xx) )
+}
+
+##' @export
+hex.integer <- function(x) {
+  vapply(x, FUN.VALUE=character(1), function(xx) .Call(Cint2hex, xx) )
+}
+
+##' @export
+hex.logical <- function(x) {
+  vapply(x, FUN.VALUE=character(1), function(xx) .Call(Cint2hex, xx) )
 }
--- src/doublehex.c
+++ src/doublehex.c
@@ -1,12 +1,24 @@
+#define USE_RINTERNALS
+
 #include <R.h>
 #include <Rinternals.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+#define STATIC_ASSERT(x) \
+  do { \
+    const static char __attribute__((__unused__)) dummy[x ? 1 : -1]; \
+  } while (0)
+#else
+#define STATIC_ASSERT(x) \
+  do { \
+    const static char dummy[x ? 1 : -1]; \
+  } while (0)
+#endif
 
 // [[export]]
 SEXP double2hex(SEXP x) {
   
-  if (sizeof(unsigned long long) != sizeof(double)) {
-    error("this code assumes sizeof(unsigned long long) and sizeof(double) are identical");
-  }
+  STATIC_ASSERT( sizeof(unsigned long long) == sizeof(double) );
   
   // double is 8 bytes, each byte can be represented by 2 hex chars,
   // so need a str with 16+1 slots
@@ -14,12 +26,23 @@ SEXP double2hex(SEXP x) {
   
   unsigned long long *xx = (unsigned long long*) REAL(x);
   char buf[n];
-  for (int i=0; i < n-1; ++i) {
-    buf[i] = '0';
-  }
-  snprintf(buf, n, "%llX", *xx);
+  snprintf(buf, n, "%016llX", *xx);
   SEXP output = PROTECT(allocVector(STRSXP, 1));
   SET_STRING_ELT(output, 0, mkChar(buf));
   UNPROTECT(1);
   return output;
 }
+
+// [[export]]
+SEXP int2hex(SEXP x) {
+  
+  int n = sizeof(int) * 2 + 1;
+  char buf[n];
+  snprintf(buf, n, "%08X", *(unsigned int*)INTEGER(x));
+  SEXP output = PROTECT(allocVector(STRSXP, 1));
+  SET_STRING_ELT(output, 0, mkChar(buf));
+  UNPROTECT(1);
+  return output;
+}
+
+#undef USE_RINTERNALS
