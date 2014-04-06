3ba98f1ce954b5fe84bc93e52d7b9493f01cca23
Kevin Ushey <kevinushey@gmail.com>
2014-04-06 12:36:04 -0700

    Only run tests if awk is available

--- tests/testthat/test-awk.R
+++ tests/testthat/test-awk.R
@@ -1,6 +1,3 @@
-library(Kmisc)
-library(testthat)
-
 dat <- data.frame(x=c(1, 2, 3), y=c('a', 'b', 'c'), z=c(10L, 11L, 12L))
 file <- tempfile()
 file2 <- paste(file, "_sub", sep='')
@@ -11,9 +8,12 @@ write.table(dat, file=file,
             quote=FALSE
 )
 
-dat2 <- read.table( text=awk('print $0', file=file) )
-expect_true( all(dat == dat2) )
-
-awk('print $1', file=file, out=file2)
-dat3 <- read.table( file2, header=FALSE )
-expect_true( all(dat[,1] == dat3[,1]) )
+tryAwk <- try(awk('print $0', file=file), silent=TRUE)
+if (!inherits(tryAwk, "try-error")) {
+  dat2 <- read.table( text=awk('print $0', file=file) )
+  expect_true( all(dat == dat2) )
+  
+  awk('print $1', file=file, out=file2)
+  dat3 <- read.table(file2, header=FALSE)
+  expect_true( all(dat[,1] == dat3[,1]) )
+}
