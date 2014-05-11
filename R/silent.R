92070284f64089b951e974ffe5d96f8a620149b9
Kevin Ushey <kevinushey@gmail.com>
2014-05-11 14:48:32 -0700

    Update docs for silent

--- R/silent.R
+++ R/silent.R
@@ -1,15 +1,15 @@
-##' Hide All Potential Output from an Expression
-##' 
-##' This function combines hides any output a function might try to write
+##' Hide All Potential Output when Evaluating an Expression
+##'
+##' This function hides any output a function might try to write
 ##' to \code{stdout}, \code{stderr}, to the \R console as a message, warning,
 ##' and so on.
-##' 
+##'
 ##' @param expr An \R expression.
 ##' @export
 silent <- function(expr) {
   call <- match.call()
-  suppressMessages( suppressWarnings(
+  suppressMessages(suppressWarnings(
     capture.output(output <- eval(call$expr, envir=parent.frame(1), enclos=baseenv()))
-  ) )
+  ))
   return(output)
 }
