d288d3269a0dd0d637097b52830bd79784fffa26
Kevin Ushey <kevinushey@gmail.com>
2018-01-08 12:29:30 -0800

    update Rcpp

--- src/Kmisc_init.c
+++ src/Kmisc_init.c
@@ -28,22 +28,22 @@ SEXP str_rev( SEXP x );
 SEXP str_slice(SEXP x, SEXP n);
 SEXP transpose_list(SEXP x_);
 SEXP unmelt(SEXP data, SEXP uniq_id, SEXP other_ind_, SEXP id_ind_, SEXP value_ind_);
-SEXP Kmisc_char_to_factor(SEXP x_SEXP, SEXP inplaceSEXP);
-SEXP Kmisc_tableRcpp(SEXP xSEXP);
-SEXP Kmisc_counts(SEXP xSEXP);
-SEXP Kmisc_extract_rows_from_file_to_file(SEXP input_file_nameSEXP, SEXP output_file_nameSEXP, SEXP delimSEXP, SEXP items_to_keepSEXP, SEXP column_to_checkSEXP);
-SEXP Kmisc_extract_rows_from_file(SEXP input_file_nameSEXP, SEXP delimSEXP, SEXP items_to_keepSEXP, SEXP column_to_checkSEXP);
-SEXP Kmisc_fast_factor(SEXP xSEXP, SEXP levelsSEXP);
-SEXP Kmisc_matches(SEXP xSEXP);
-SEXP Kmisc_read(SEXP pathSEXP, SEXP linesSEXP);
-SEXP Kmisc_split_file(SEXP pathSEXP, SEXP dirSEXP, SEXP basenameSEXP, SEXP path_sepSEXP, SEXP sepSEXP, SEXP prependSEXP, SEXP file_extSEXP, SEXP columnSEXP, SEXP skipSEXP, SEXP verboseSEXP);
-SEXP Kmisc_split_runs_numeric(SEXP XSEXP);
-SEXP Kmisc_split_runs_character(SEXP XSEXP);
-SEXP Kmisc_split_runs_one(SEXP xSEXP);
-SEXP Kmisc_split(SEXP xSEXP, SEXP ySEXP, SEXP na_last_SEXP);
-SEXP Kmisc_stack_list_df(SEXP XSEXP, SEXP classesSEXP, SEXP num_elemSEXP, SEXP make_row_namesSEXP, SEXP nameSEXP, SEXP keep_list_indexSEXP, SEXP index_nameSEXP);
-SEXP Kmisc_str_collapse_list(SEXP xSEXP);
-SEXP Kmisc_str_collapse_str(SEXP xSEXP);
+SEXP _Kmisc_char_to_factor(SEXP x_SEXP, SEXP inplaceSEXP);
+SEXP _Kmisc_tableRcpp(SEXP xSEXP);
+SEXP _Kmisc_counts(SEXP xSEXP);
+SEXP _Kmisc_extract_rows_from_file_to_file(SEXP input_file_nameSEXP, SEXP output_file_nameSEXP, SEXP delimSEXP, SEXP items_to_keepSEXP, SEXP column_to_checkSEXP);
+SEXP _Kmisc_extract_rows_from_file(SEXP input_file_nameSEXP, SEXP delimSEXP, SEXP items_to_keepSEXP, SEXP column_to_checkSEXP);
+SEXP _Kmisc_fast_factor(SEXP xSEXP, SEXP levelsSEXP);
+SEXP _Kmisc_matches(SEXP xSEXP);
+SEXP _Kmisc_read(SEXP pathSEXP, SEXP linesSEXP);
+SEXP _Kmisc_split_file(SEXP pathSEXP, SEXP dirSEXP, SEXP basenameSEXP, SEXP path_sepSEXP, SEXP sepSEXP, SEXP prependSEXP, SEXP file_extSEXP, SEXP columnSEXP, SEXP skipSEXP, SEXP verboseSEXP);
+SEXP _Kmisc_split_runs_numeric(SEXP XSEXP);
+SEXP _Kmisc_split_runs_character(SEXP XSEXP);
+SEXP _Kmisc_split_runs_one(SEXP xSEXP);
+SEXP _Kmisc_split(SEXP xSEXP, SEXP ySEXP, SEXP na_last_SEXP);
+SEXP _Kmisc_stack_list_df(SEXP XSEXP, SEXP classesSEXP, SEXP num_elemSEXP, SEXP make_row_namesSEXP, SEXP nameSEXP, SEXP keep_list_indexSEXP, SEXP index_nameSEXP);
+SEXP _Kmisc_str_collapse_list(SEXP xSEXP);
+SEXP _Kmisc_str_collapse_str(SEXP xSEXP);
 
 R_CallMethodDef callMethods[]  = {
   {"Cany_na", (DL_FUNC) &any_na, 1},
@@ -69,22 +69,22 @@ R_CallMethodDef callMethods[]  = {
   {"Cstr_slice", (DL_FUNC) &str_slice, 2},
   {"Ctranspose_list", (DL_FUNC) &transpose_list, 1},
   {"Cunmelt", (DL_FUNC) &unmelt, 5},
-  {"CKmisc_char_to_factor", (DL_FUNC) &Kmisc_char_to_factor, 2},
-  {"CKmisc_tableRcpp", (DL_FUNC) &Kmisc_tableRcpp, 1},
-  {"CKmisc_counts", (DL_FUNC) &Kmisc_counts, 1},
-  {"CKmisc_extract_rows_from_file_to_file", (DL_FUNC) &Kmisc_extract_rows_from_file_to_file, 5},
-  {"CKmisc_extract_rows_from_file", (DL_FUNC) &Kmisc_extract_rows_from_file, 4},
-  {"CKmisc_fast_factor", (DL_FUNC) &Kmisc_fast_factor, 2},
-  {"CKmisc_matches", (DL_FUNC) &Kmisc_matches, 1},
-  {"CKmisc_read", (DL_FUNC) &Kmisc_read, 2},
-  {"CKmisc_split_file", (DL_FUNC) &Kmisc_split_file, 10},
-  {"CKmisc_split_runs_numeric", (DL_FUNC) &Kmisc_split_runs_numeric, 1},
-  {"CKmisc_split_runs_character", (DL_FUNC) &Kmisc_split_runs_character, 1},
-  {"CKmisc_split_runs_one", (DL_FUNC) &Kmisc_split_runs_one, 1},
-  {"CKmisc_split", (DL_FUNC) &Kmisc_split, 3},
-  {"CKmisc_stack_list_df", (DL_FUNC) &Kmisc_stack_list_df, 7},
-  {"CKmisc_str_collapse_list", (DL_FUNC) &Kmisc_str_collapse_list, 1},
-  {"CKmisc_str_collapse_str", (DL_FUNC) &Kmisc_str_collapse_str, 1},
+  {"CKmisc_char_to_factor", (DL_FUNC) &_Kmisc_char_to_factor, 2},
+  {"CKmisc_tableRcpp", (DL_FUNC) &_Kmisc_tableRcpp, 1},
+  {"CKmisc_counts", (DL_FUNC) &_Kmisc_counts, 1},
+  {"CKmisc_extract_rows_from_file_to_file", (DL_FUNC) &_Kmisc_extract_rows_from_file_to_file, 5},
+  {"CKmisc_extract_rows_from_file", (DL_FUNC) &_Kmisc_extract_rows_from_file, 4},
+  {"CKmisc_fast_factor", (DL_FUNC) &_Kmisc_fast_factor, 2},
+  {"CKmisc_matches", (DL_FUNC) &_Kmisc_matches, 1},
+  {"CKmisc_read", (DL_FUNC) &_Kmisc_read, 2},
+  {"CKmisc_split_file", (DL_FUNC) &_Kmisc_split_file, 10},
+  {"CKmisc_split_runs_numeric", (DL_FUNC) &_Kmisc_split_runs_numeric, 1},
+  {"CKmisc_split_runs_character", (DL_FUNC) &_Kmisc_split_runs_character, 1},
+  {"CKmisc_split_runs_one", (DL_FUNC) &_Kmisc_split_runs_one, 1},
+  {"CKmisc_split", (DL_FUNC) &_Kmisc_split, 3},
+  {"CKmisc_stack_list_df", (DL_FUNC) &_Kmisc_stack_list_df, 7},
+  {"CKmisc_str_collapse_list", (DL_FUNC) &_Kmisc_str_collapse_list, 1},
+  {"CKmisc_str_collapse_str", (DL_FUNC) &_Kmisc_str_collapse_str, 1},
   {NULL, NULL, 0}
 };
 
